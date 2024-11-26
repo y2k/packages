9db3a3875a69699e944827e2245c71672c8104c0
y2k <itwisterlx@gmail.com>
2024-11-26 11:48:16 +0100

    add interpreter package

---
+++ interpreter/java/0.1.0/interpreter.clj
@@ -0,0 +1,21 @@
+(ns interpreter (:import [java.util.function Function]))
+
+(defn- ^Function function [^Function f] f)
+
+(defn make_env []
+  {:scope
+   {:+ (function (fn [[^int a ^int b]] (+ a b)))}})
+1
+(defn eval [env node]
+  (cond
+    (list? node)
+    (let [name (first node)
+          args (rest node)
+          ^Function f (get (:scope env) name)]
+      (.apply f args))
+
+    (vector? node) (FIXME)
+
+    (map? node) (FIXME)
+
+    :else node))
