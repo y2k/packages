9b915cc7eef4826dbe34082e8419f196fc48cf37
y2k <itwisterlx@gmail.com>
2024-11-30 18:19:34 +0100

    update interpreter

--- interpreter/java/0.1.0/interpreter.clj
+++ interpreter/java/0.1.0/interpreter.clj
@@ -4,10 +4,13 @@
 
 (defn make_env []
   {:scope
-   {:+ (function (fn [[^String a ^String b]] (+ (Integer/parseInt a) (Integer/parseInt b))))}})
+   {:+ (function (fn [[a b]]
+                   (let [aa (as (if (is a String) (Integer/parseInt (as a String)) a) int)
+                         bb (as (if (is b String) (Integer/parseInt (as b String)) b) int)]
+                     (+ aa bb))))}})
 
 ;; (defn- invoke_ [env name args]
-;;   ;; (println "ENV:" env)
+;;   (println "INVOKE:" name args env)
 ;;   (let [^Function f (get (:scope env) name)]
 ;;     (if (= null f)
 ;;       (FIXME name)
@@ -47,29 +50,52 @@
   (let [r (get (:scope env) name)]
     (if (= null r) name r)))
 
+(defn- eval_do_body [env lexemes]
+  ;; (println "EVAL_DO_BODY:" lexemes)
+  (let [[r env2 lx2] (eval env lexemes)]
+    (if (empty? lx2)
+      [r env2 lx2]
+      (eval_do_body env2 lx2))))
+
+(defn- get_lambda_body [^int level buffer lx]
+  (let [node (first lx)]
+    (case node
+      "(" (get_lambda_body (+ level 1) (conj buffer node) (rest lx))
+      ")" (if (= level 0)
+            [buffer lx]
+            (get_lambda_body (- level 1) (conj buffer node) (rest lx)))
+      (get_lambda_body level (conj buffer node) (rest lx)))))
+
 ;; -> [_ Env Lexemes]
 (defn eval [env lexemes]
+  ;; (println "EVAL:" lexemes "\n" env)
   (let [node (first lexemes)]
     (if (= "(" node)
       (let [[f env2 lexemes2] (eval env (rest lexemes))]
         (case f
+          "do" (eval_do_body env2 lexemes2)
           "def" (let [name (first lexemes2)
                       [value env3 lexemes3] (eval env (rest lexemes2))]
                   [null (register_value env3 name value) lexemes3])
+          "bind*" (let [name (first lexemes2)
+                        [value env3 lexemes3] (eval env (rest lexemes2))]
+                    [null (register_value env3 name value) lexemes3])
           ;; Abstraction
-          "fn*" (let [[args_names lexemes3] (get_function_args_names lexemes2)]
+          "fn*" (let [[args_names lexemes3] (get_function_args_names lexemes2)
+                      [body_lx lx4] (get_lambda_body 0 [] lexemes3)]
                   [(function (fn [args]
-                              ;;  (println "ARGS:" args)
                                (let [env3 (merge_args_with_values env2 args_names args)]
-                                ;;  (println "CALL:" env2 env3)
-                                 (first (eval env3 lexemes3)))))
+                                ;;  (println "CALL LAMBDA:" args env3 body_lx)
+                                 (first (eval_do_body env3 body_lx)))))
                    env2
-                   lexemes3])
+                   lx4])
           ;; Application
           (let [[args lexemes3] (parse_all_args env2 lexemes2)]
+            ;; (println "CALL1:" (first (rest lexemes)) args f env2)
             [(if (is f Function)
                (.apply (as f Function) args)
                ;; (invoke_ env2 f args)
-               (FIXME f args))
-             env2 lexemes3])))
+               (FIXME f " " args))
+             env2
+             lexemes3])))
       [(resolve_value env node) env (rest lexemes)])))
