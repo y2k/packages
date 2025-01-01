ab441c0b34a6b240e8715ef42d98629e4cd0c6a8
y2k <itwisterlx@gmail.com>
2025-01-01 12:30:08 +0100

    update build

--- build/0.1.0/main.clj
+++ build/0.1.0/main.clj
@@ -1,12 +1,24 @@
 (defn default []
-    "#!/bin/bash\nset -e\nset -u\nset -o pipefail\nexport OCAMLRUNPARAM=b\n")
+  "#!/bin/bash\nset -e\nset -u\nset -o pipefail\nexport OCAMLRUNPARAM=b\n")
 
-  (defn build [target root_ns rules]
-    (let [res (map
-               (fn [r]
-                 (str "clj2js compile"
-                      " -root_ns " root_ns
-                      " -target " target
-                      " -src " (:src r) " > " (:target r)))
-               rules)]
-      (reduce (fn [a x] (str a "\n" x)) "" res)))
+(defn build-files [{target :target root-ns :root-ns rules :rules}]
+  (let [res (map
+             (fn [r]
+               (str "clj2js compile"
+                    (if (some? root-ns)
+                      (str " -root_ns " root-ns)
+                      "")
+                    " -target " target
+                    " -src " (:src r) " > " (:target r)))
+             rules)]
+    (reduce (fn [a x] (str a "\n" x)) "" res)))
+
+(defn build-java-package [{root-ns :root-ns target-dir :target-dir items :items}]
+  (build-files
+   {:target "java"
+    :root-ns root-ns
+    :rules (map
+            (fn [x]
+              {:src (str root-ns "/" x ".clj")
+               :target (str target-dir "/" root-ns "/" x ".java")})
+            items)}))
