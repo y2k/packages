2e80f3026fa949fccf84c8d002ab9f6880783e07
y2k <itwisterlx@gmail.com>
2025-01-09 15:47:43 +0100

    add make

---
+++ make/0.1.0/main.clj
@@ -0,0 +1,52 @@
+(defn- default [] "# THIS FILE IS GENERATED\n\n")
+
+(defn- build-files [{target :target root-ns :root-ns rules :rules}]
+  (let [res (map
+             (fn [r]
+               (str (:target r) ": " (:src r) "\n"
+                    "\t@ mkdir -p $$(dirname " (:target r) ")\n"
+                    "\t@ clj2js compile"
+                    (if (some? root-ns)
+                      (str " -root_ns " root-ns)
+                      "")
+                    " -target " target
+                    " -src " (:src r) " > " (:target r)))
+             rules)]
+    (reduce (fn [a x] (str a "\n\n" x)) "" res)))
+
+(defn build [{lang :lang src-dir :src-dir target-dir :target-dir items :items}]
+  {:target lang
+   :rules (map
+           (fn [x]
+             {:src (str src-dir "/" x ".clj")
+              :target (str target-dir "/" x "." lang)})
+           items)})
+
+(defn vendor [params]
+  {:target (:lang params)
+   :rules (->>
+           (:items params)
+           (map
+            (fn [x]
+              {:src (str "vendor/" (:name x) "/" (:version x) "/main.clj")
+               :target (str (:target-dir params) "/" (:name x) "/main.js")})))})
+
+(defn- make_all_target [xs]
+  (reduce
+   (fn [acc x]
+     (str acc
+          (reduce
+           (fn [ac2 r] (str ac2 " " (:target r)))
+           ""
+           (:rules x))))
+   "all:"
+   xs))
+
+(defn root [xs]
+  (reduce
+   (fn [acc x]
+     (str acc (build-files x)))
+   (str
+    (default)
+    (make_all_target xs))
+   xs))
