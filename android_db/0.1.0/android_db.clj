fa7ff24cbd4668c8048637f648d0522d2a109a02
y2k <itwisterlx@gmail.com>
2025-04-10 19:22:07 +0200

    add android_db

---
+++ android_db/0.1.0/android_db.clj
@@ -0,0 +1,15 @@
+(ns _ (:import [io.requery.android.database.sqlite SQLiteDatabase]))
+
+(defn query [sql args]
+  (fn [w] ((:android_db:query w) sql args)))
+
+(defn- query_ [db sql args]
+  (let [c (.rawQuery (cast SQLiteDatabase db)
+                     (:sql sql)
+                     (into-array2 (class String) (:args args)))
+        result (if (.moveToFirst c) (.getString c 0) nil)]
+    result))
+
+(defn attach_effect_handler [{db_source :db} env]
+  (let [^SQLiteDatabase db (SQLiteDatabase/openOrCreateDatabase (cast String db_source) nil)]
+    (assoc env :android_db:query (fn [sql args] (query_ db sql args)))))
