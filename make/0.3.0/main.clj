a5b9fe5ed64e5a2c82bfe5e5ccb3eda7b7ce6c4f
y2k <itwisterlx@gmail.com>
2025-06-13 12:02:31 +0200

    make: clear code

--- make/0.3.0/main.clj
+++ make/0.3.0/main.clj
@@ -1,31 +1,8 @@
-(comment
-  (defn generate [config]
-    (let [files (string/split (string/trim STDIN) "\n")]
-      (->>
-       config
-       (map
-        (fn [cfg]
-          (->>
-           files
-           (filter (fn [x]
-                     (string/starts-with? x (str "./" (:root cfg)))))
-           (map
-            (fn [x]
-              (let [[name] (re-find "./([0-9A-Za-z/_\.-]+).clj" x)
-                    file (str (:out-dir cfg) "/" name "." (:target cfg))]
-                (str file ":\n\t@ ly2k -target java -src " x " > " file))))
-           (reduce
-            (fn [a x] (str a "\n\n" x)) ""))))
-       (reduce
-        (fn [a x] (str a x))
-        "# GENERATED FILE - DO NOT EDIT")))))
-
-(defn generate [config]
+(defn generate [rules]
   (->>
-   config
+   rules
    (map
     (fn [cfg]
-;;
       (str
        ;; (:out-dir cfg) "/%." (:target cfg) ": ./%.clj\n"
        ;; $(OUT_DIR)/%: $(SRC_DIR)/%.clj\n
