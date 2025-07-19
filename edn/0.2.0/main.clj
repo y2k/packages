67d322503a10a58ccca3802f84822fb38a35225e
y2k <itwisterlx@gmail.com>
2025-07-19 13:00:20 +0200

    add effects-tools

--- edn/0.2.0/main.clj
+++ edn/0.2.0/main.clj
@@ -3,12 +3,17 @@
 (defn to_string [x]
   (cond
     (nil? x) "nil"
-    (fn? x) "nil"
+    ;; (symbol? x) (.-value x)
     (number? x) (str x)
     (boolean? x) (if x "true" "false")
     (string? x) (if (.test symbol_regex x)
                   (str ":" x)
-                  (str "\"" x "\""))
+                  (str "\""
+                       (.replaceAll
+                        (.replaceAll x "\\" "\\\\")
+                        "\"" "\\\"")
+                       "\""))
+
     (vector? x)
     (str "["
          (reduce (fn [a n]
@@ -18,14 +23,22 @@
                  ""
                  x)
          "]")
+
+    ;; (list? x)
+    ;; (str "("
+    ;;      (reduce (fn [a n]
+    ;;                (if (= "" a)
+    ;;                  (to_string n)
+    ;;                  (str a " " (to_string n))))
+    ;;              ""
+    ;;              x)
+    ;;      ")")
+
     :else (str "{"
                (reduce (fn [a [k n]]
-                         (let [value (to_string n)]
-                           (if (= value "nil")
-                             a
-                             (if (= "" a)
-                               (str (to_string k) " " value)
-                               (str a " " (to_string k) " " value)))))
+                         (if (= "" a)
+                           (str (to_string k) " " (to_string n))
+                           (str a " " (to_string k) " " (to_string n))))
                        ""
                        x)
                "}")))
