7f4ee37ca955dba8dc6d023d42402791497805a1
y2k <itwisterlx@gmail.com>
2025-07-20 14:37:41 +0200

    fix package naming, add android scafold

---
+++ android_app/0.1.0/index.clj
@@ -0,0 +1,36 @@
+(ns _ (:require
+       ["../chat_ui/chat_ui" :as ui]
+       ["../../main" :as m])
+    (:import [android.app Activity]
+             [android.content Intent]
+             [android.view View]
+             [android.os Bundle]))
+
+(gen-class
+ :name MainActivity
+ :extends Activity
+ :prefix "activity_"
+ :fields ["w_atom"]
+ :init "init"
+ :methods [[^Override onCreate [Bundle] void]
+           [^Override onActivityResult [int int Intent] void]])
+
+(defn- activity_init [^MainActivity self]
+  (set! (.-w_atom self) (atom {})))
+
+(defn- activity_onCreate [^MainActivity self ^Bundle bundle]
+  (let [root (cast View (ui/root_ self))
+        w_atom (.-w_atom self)]
+    (.setContentView self root)
+    (ui/add_effect_handlers self root w_atom)
+    (m/main :on-create
+            {:context self
+             :state w_atom})))
+
+(defn- activity_onActivityResult [^MainActivity self ^int requestCode ^int resultCode ^Intent data]
+  (m/main :on-activity-result
+          {:context self
+           :state (.-w_atom self)
+           :request-code requestCode
+           :result-code resultCode
+           :data data}))
--- effects_tools/0.1.0/date.clj
+++ effects_tools/0.1.0/date.clj
@@ -1,6 +1,9 @@
 (defn get_now [] (fn [w]
                    ((:now:now w))))
 
+(defn get_unixtime [] (fn [w]
+                        ((:now:now w))))
+
 (defn effect_handler [w]
   (assoc w :now:now (fn [] [(unixtime) nil])))
 
@@ -12,5 +15,5 @@
 (defn mock_effect_handler [time w]
   (assoc w :now:now (fn [] [(deref time) nil])))
 
-(defn shift [time offset]
-  (swap! time (fn [t] (+ t offset))))
+(defn shift [time ^double offset]
+  (swap! time (fn [^double t] (+ t offset))))
